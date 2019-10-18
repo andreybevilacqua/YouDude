72e6accadf6e8c9b3832b3e78713b786addf6615
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-10-18 11:41:11 +0200

    First commit

---
+++ src/test/java/com/abevilacqua/youdude/YoududeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abevilacqua.youdude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoududeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
