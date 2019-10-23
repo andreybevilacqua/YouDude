0d02e958648e00a6d392dc1ad610723645fd991b
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-10-23 16:39:49 +0200

    Removes post method

--- src/main/java/com/abevilacqua/youdude/controller/level_0/YouDudeController.java
+++ src/main/java/com/abevilacqua/youdude/controller/level_0/YouDudeController.java
@@ -1,12 +1,10 @@
 package com.abevilacqua.youdude.controller.level_0;
 
-import com.abevilacqua.youdude.model.Channel;
 import com.abevilacqua.youdude.service.ChannelService;
 import com.abevilacqua.youdude.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/youdude_level0")
@@ -21,21 +19,5 @@ public class YouDudeController {
                            ChannelService channelService) {
     this.userService = userService;
     this.channelService = channelService;
-  }
-
-  @PostMapping
-  public ResponseEntity yoududeService(@RequestParam(value = "action") String action,
-                                       @RequestParam(value = "user_id", required = false) long user_id,
-                                       @RequestBody(required = false) Channel channel) {
-    switch (action) {
-      case "getUsers":
-        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
-      case "getChannelsPerUser":
-        return new ResponseEntity<>(channelService.getAllChannelsFromUser(user_id), HttpStatus.OK);
-      case "createChannel":
-        return new ResponseEntity<>(channelService.createChannel(channel), HttpStatus.CREATED);
-      default:
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
   }
 }
