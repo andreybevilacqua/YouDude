7416ad3db4b33630d4fc616e6813ef69a1941d0c
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-11-23 17:52:55 +0100

    Adds PlaylistDTO. Map all objects on PlaylistController to DTO

---
+++ src/main/java/com/abevilacqua/youdude/controller/dto/PlaylistDTO.java
@@ -0,0 +1,39 @@
+package com.abevilacqua.youdude.controller.dto;
+
+import com.abevilacqua.youdude.model.Playlist;
+import com.abevilacqua.youdude.model.Video;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+public class PlaylistDTO {
+
+  private final long playlist_id;
+
+  private final String name;
+
+  private final List<Long> videos_id;
+
+  private final long user_id;
+
+  public static PlaylistDTO mapper(Playlist playlist) {
+    List videos = playlist
+        .getVideos()
+        .stream()
+        .map(Video::getId)
+        .collect(Collectors.toList());
+
+    return PlaylistDTO.builder()
+        .playlist_id(playlist.getId())
+        .name(playlist.getName())
+        .videos_id(videos)
+        .user_id(playlist.getUser().getId())
+        .build();
+
+  }
+
+}
--- src/main/java/com/abevilacqua/youdude/controller/level_2/PlaylistController.java
+++ src/main/java/com/abevilacqua/youdude/controller/level_2/PlaylistController.java
@@ -1,5 +1,6 @@
 package com.abevilacqua.youdude.controller.level_2;
 
+import com.abevilacqua.youdude.controller.dto.PlaylistDTO;
 import com.abevilacqua.youdude.model.Playlist;
 import com.abevilacqua.youdude.service.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.abevilacqua.youdude.controller.dto.PlaylistDTO.mapper;
 
 @RestController
 @RequestMapping("/level2/playlists")
@@ -23,23 +27,30 @@ public class PlaylistController {
   }
 
   @GetMapping
-  public ResponseEntity<List<Playlist>> getAllPlaylists() {
-    return new ResponseEntity<>(playlistService.getAllPlaylists(), HttpStatus.OK);
+  public ResponseEntity<List<PlaylistDTO>> getAllPlaylists() {
+    return new ResponseEntity<>(playlistService.getAllPlaylists()
+        .stream()
+        .map(PlaylistDTO::mapper)
+        .collect(Collectors.toList()), HttpStatus.OK);
   }
 
   @GetMapping("{user_id}")
-  public ResponseEntity<List<Playlist>> getPlaylistPerUser(@PathVariable("user_id") long user_id) {
-    return new ResponseEntity<>(playlistService.getAllFromUser(user_id), HttpStatus.OK);
+  public ResponseEntity<List<PlaylistDTO>> getPlaylistPerUser(@PathVariable("user_id") long user_id) {
+    return new ResponseEntity<>(playlistService.getAllFromUser(user_id)
+        .stream()
+        .map(PlaylistDTO::mapper)
+        .collect(Collectors.toList()), HttpStatus.OK);
   }
 
   @PostMapping
-  public ResponseEntity<Playlist> createPlayList(@RequestBody Playlist playlist) {
-    return new ResponseEntity<>(playlistService.createPlaylist(playlist), HttpStatus.CREATED);
+  public ResponseEntity<PlaylistDTO> createPlayList(@RequestBody Playlist playlist) {
+    return new ResponseEntity<>(mapper(playlistService.createPlaylist(playlist)), HttpStatus.CREATED);
   }
 
   @DeleteMapping("{playlist_id}")
-  public ResponseEntity<Playlist> deletePlaylist(@PathVariable(value = "playlist_id") long playlist_id) {
+  public ResponseEntity<PlaylistDTO> deletePlaylist(@PathVariable(value = "playlist_id") long playlist_id) {
     return playlistService.deletePlaylist(playlist_id)
+        .map(PlaylistDTO::mapper)
         .map(playlist -> new ResponseEntity<>(playlist, HttpStatus.OK))
         .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
