5ae4c3ba255d5eb6dc70d2400ef98eca7046ae7d
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-11-26 22:36:41 +0100

    Adds CompletableFuture from java.util.concurrent to UserService

--- src/main/java/com/abevilacqua/youdude/service/UserService.java
+++ src/main/java/com/abevilacqua/youdude/service/UserService.java
@@ -3,10 +3,14 @@ package com.abevilacqua.youdude.service;
 import com.abevilacqua.youdude.model.User;
 import com.abevilacqua.youdude.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.concurrent.CompletableFuture.completedFuture;
 
 @Service
 public class UserService {
@@ -16,12 +20,19 @@ public class UserService {
   @Autowired
   public UserService(UserRepo userRepo) { this.userRepo = userRepo; }
 
-  public List<User> getAllUsers() {
-    return userRepo.findAll();
+  @Async
+  public CompletableFuture<List<User>> getAllUsers() {
+    return completedFuture(userRepo.findAll());
   }
 
-  public Optional<User> getById(long id) { return userRepo.findById(id);}
+  @Async
+  public CompletableFuture<Optional<User>> getById(long id) {
+    return completedFuture(userRepo.findById(id));
+  }
 
-  public User createUser(User user) { return userRepo.save(user); }
+  @Async
+  public CompletableFuture<User> createUser(User user) {
+    return completedFuture(userRepo.save(user));
+  }
 
 }
