47dacf94a004718dfb360d45458418c29f079f9a
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-11-26 22:41:06 +0100

    Adds @EnableAsync and adds new Executor bean

--- src/main/java/com/abevilacqua/youdude/YouDudeApplication.java
+++ src/main/java/com/abevilacqua/youdude/YouDudeApplication.java
@@ -8,8 +8,13 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class YouDudeApplication {
 
 	public static void main(String[] args) {
@@ -21,6 +26,17 @@ public class YouDudeApplication {
 									PlaylistRepo playlistRepo,
 									VideoRepo videoRepo) {
 		return DBInitializer.initDB(userRepo, playlistRepo, videoRepo);
+	}
+
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("GithubLookup-");
+		executor.initialize();
+		return executor;
 	}
 
 }
