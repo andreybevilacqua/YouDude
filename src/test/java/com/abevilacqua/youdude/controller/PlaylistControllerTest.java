69ddca45e06a5327f25650c8f109520e7a59fe0c
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-11-26 17:00:56 +0100

    Adds more integration test in playlist controller test

--- src/test/java/com/abevilacqua/youdude/controller/PlaylistControllerTest.java
+++ src/test/java/com/abevilacqua/youdude/controller/PlaylistControllerTest.java
@@ -1,9 +1,12 @@
 package com.abevilacqua.youdude.controller;
 
 import com.abevilacqua.youdude.controller.level_2.PlaylistController;
+import com.abevilacqua.youdude.model.Playlist;
+import com.abevilacqua.youdude.model.User;
+import com.abevilacqua.youdude.model.Video;
 import com.abevilacqua.youdude.service.PlaylistService;
 import com.abevilacqua.youdude.service.UserService;
-import com.abevilacqua.youdude.utils.ObjectHelper;
+import com.abevilacqua.youdude.service.VideoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +17,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static com.abevilacqua.youdude.utils.ObjectHelper.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,6 +37,9 @@ class PlaylistControllerTest {
   @Autowired
   private UserService userService;
 
+  @Autowired
+  private VideoService videoService;
+
   private MockMvc mockMvc;
 
   private final String URL = "/level2/playlists";
@@ -35,13 +47,25 @@ class PlaylistControllerTest {
   @BeforeEach
   void setup() {
     PlaylistController playlistController = new PlaylistController(playlistService);
-    mockMvc = ObjectHelper.createMockMvc(playlistController);
+    mockMvc = createMockMvc(playlistController);
   }
 
   @Test
   @DisplayName("Should find all playlists")
   void shouldFindAllPlaylists() throws Exception {
     mockMvc.perform(get(URL)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].playlist_id").isNumber())
+        .andExpect(jsonPath("$[0].name").isString())
+        .andExpect(jsonPath("$[0].videos_id").isArray())
+        .andExpect(jsonPath("$[0].user_id").isNumber());
+  }
+
+  @Test
+  @DisplayName("Should find all playlists from an user")
+  void shouldFindAllPlaylistsFromUser() throws Exception {
+    mockMvc.perform(get(URL + "/1")
         .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$[0].playlist_id").isNumber())
