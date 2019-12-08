9482506faeef94de17afbdb5347f4822d3d208b5
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-12-08 22:55:09 +0100

    Creates builder for PageImplDTO

--- src/main/java/com/abevilacqua/youdude/controller/dto/PageImplDTO.java
+++ src/main/java/com/abevilacqua/youdude/controller/dto/PageImplDTO.java
@@ -1,6 +1,5 @@
 package com.abevilacqua.youdude.controller.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -9,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @Getter
-@Builder
 public class PageImplDTO<T> extends PageImpl<T> {
 
   private List<T> content;
@@ -22,18 +20,14 @@ public class PageImplDTO<T> extends PageImpl<T> {
 
   public PageImplDTO(List<T> content, Pageable pageable, long total) {
     super(content, pageable, total);
+  }
 
-    this.content = content;
-    this.totalPages = super.getTotalPages();
-    this.totalElements = super.getTotalElements();
-    this.first = super.isFirst();
-    this.last = super.isLast();
-    this.size = super.getSize();
-    this.numberOfElements = super.getNumberOfElements();
+  public PageImplDTO() {
+    super(null, null, 0);
   }
 
   public static PageImplDTO pageMapper(Page pageImpl) {
-    return PageImplDTO.builder()
+    return new PageImplDTO.Builder()
         .content(pageImpl.getContent())
         .totalElements(pageImpl.getTotalElements())
         .first(pageImpl.isFirst())
@@ -42,5 +36,65 @@ public class PageImplDTO<T> extends PageImpl<T> {
         .size(pageImpl.getSize())
         .numberOfElements(pageImpl.getNumberOfElements())
         .build();
+  }
+
+  public static class Builder {
+
+    private List content;
+
+    private long totalElements;
+
+    private boolean first, last;
+
+    private int totalPages, size, numberOfElements;
+
+    public Builder content(List content) {
+      this.content = content;
+      return this;
+    }
+
+    public Builder totalElements(long totalElements) {
+      this.totalElements = totalElements;
+      return this;
+    }
+
+    public Builder first(boolean first) {
+      this.first = first;
+      return this;
+    }
+
+    public Builder last(boolean last) {
+      this.last = last;
+      return this;
+    }
+
+    public Builder totalPages(int totalPages) {
+      this.totalPages = totalPages;
+      return this;
+    }
+
+    public Builder size(int size) {
+      this.size = size;
+      return this;
+    }
+
+    public Builder numberOfElements(int numberOfElements) {
+      this.numberOfElements = numberOfElements;
+      return this;
+    }
+
+    public PageImplDTO build() {
+      PageImplDTO pageImplDTO = new PageImplDTO();
+      pageImplDTO.content = this.content;
+      pageImplDTO.totalElements = this.totalElements;
+      pageImplDTO.first = this.first;
+      pageImplDTO.last = this.last;
+      pageImplDTO.totalPages = this.totalPages;
+      pageImplDTO.size = this.size;
+      pageImplDTO.numberOfElements = numberOfElements;
+
+      return pageImplDTO;
+    }
+
   }
 }
