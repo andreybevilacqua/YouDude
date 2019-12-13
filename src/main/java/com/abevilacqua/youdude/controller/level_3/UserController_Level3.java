a007da8d43bff1e86afe03d810396de7ff017a5b
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-12-13 11:30:18 +0100

    Adds WebMvcLinkBuilder to build a self-relation link in the getAllUsers HTTP response

--- src/main/java/com/abevilacqua/youdude/controller/level_3/UserController_Level3.java
+++ src/main/java/com/abevilacqua/youdude/controller/level_3/UserController_Level3.java
@@ -7,16 +7,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.core.ControllerEntityLinksFactoryBean;
+import org.springframework.hateoas.server.mvc.ControllerLinkBuilder;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
-import static com.abevilacqua.youdude.controller.level_3.link.LinkHelper.getUserPath;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/level3/users")
@@ -37,7 +39,10 @@ public class UserController_Level3 {
     CompletableFuture<Page<User>> completableFuture = userService.getAllUsers(page, size, sortBy);
     Page<User> userPage = completableFuture.join();
     CollectionModel<EntityModel<User>> recentResources = CollectionModel.wrap(userPage.getContent());
-    recentResources.add(new Link(getUserPath(), "recent"));
+    recentResources.add(
+        WebMvcLinkBuilder
+            .linkTo(methodOn(UserController_Level3.class).getAllUsers(page, size, sortBy))
+            .withSelfRel());
     return new ResponseEntity<>(recentResources, HttpStatus.OK);
   }
 
