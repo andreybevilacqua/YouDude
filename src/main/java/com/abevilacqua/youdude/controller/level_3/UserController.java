124dec668eb2f1eb19dfa60fc2531799322493fe
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-12-13 11:05:25 +0100

    Adds UserController level 3 for RESTFul HATEOAS requests

---
+++ src/main/java/com/abevilacqua/youdude/controller/level_3/UserController.java
@@ -0,0 +1,44 @@
+package com.abevilacqua.youdude.controller.level_3;
+
+import com.abevilacqua.youdude.model.User;
+import com.abevilacqua.youdude.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.abevilacqua.youdude.controller.level_3.link.LinkHelper.getUserPath;
+
+@RestController
+@RequestMapping("/level3/users")
+public class UserController {
+
+  private UserService userService;
+
+  @Autowired
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping
+  public ResponseEntity<CollectionModel<EntityModel<User>>> getAllUsers(
+      @RequestParam(value = "page", defaultValue = "0") final int page,
+      @RequestParam(value = "size", defaultValue = "10") final int size,
+      @RequestParam(value = "sort", defaultValue = "id") final String sortBy) {
+    CompletableFuture<Page<User>> completableFuture = userService.getAllUsers(page, size, sortBy);
+    Page<User> userPage = completableFuture.join();
+    CollectionModel<EntityModel<User>> recentResources = CollectionModel.wrap(userPage.getContent());
+    recentResources.add(new Link(getUserPath(), "recent"));
+    return new ResponseEntity<>(recentResources, HttpStatus.OK);
+  }
+
+}
---
+++ src/main/java/com/abevilacqua/youdude/controller/level_3/link/LinkHelper.java
@@ -0,0 +1,8 @@
+package com.abevilacqua.youdude.controller.level_3.link;
+
+public final class LinkHelper {
+
+  private static final String USER_PATH = "http://localhost:8080/level3/users/";
+
+  public static String getUserPath() { return USER_PATH; }
+}
