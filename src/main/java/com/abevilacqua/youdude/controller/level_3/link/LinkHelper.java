114455de8a684b8de7be061c8745b3c30febf947
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-12-13 15:45:54 +0100

    Removes LinkHelper class

--- src/main/java/com/abevilacqua/youdude/controller/level_3/link/LinkHelper.java
+++
@@ -1,8 +0,0 @@
-package com.abevilacqua.youdude.controller.level_3.link;
-
-public final class LinkHelper {
-
-  private static final String USER_PATH = "http://localhost:8080/level3/users/";
-
-  public static String getUserPath() { return USER_PATH; }
-}
