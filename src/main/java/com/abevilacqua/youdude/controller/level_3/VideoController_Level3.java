aa9c479db0ff83e9a0d36202d6e6add889597dee
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-12-18 12:00:46 +0100

    Adds link on each user to the UserController_Level3.getUserByIdI()

--- src/main/java/com/abevilacqua/youdude/controller/level_3/VideoController_Level3.java
+++ src/main/java/com/abevilacqua/youdude/controller/level_3/VideoController_Level3.java
@@ -34,7 +34,14 @@ public class VideoController_Level3 {
 
     CollectionModel<VideoResource> collectionModel =
         new VideoResourceAssembler().toCollectionModel(videosCompletableFuture.join());
+    collectionModel.getContent().forEach(videoResource -> videoResource.getUser());
 
+    collectionModel
+        .getContent()
+        .forEach(videoResource -> videoResource
+            .add(WebMvcLinkBuilder
+                .linkTo(methodOn(UserController_Level3.class).getUserById(videoResource.getUser().getId()))
+                .withRel("user-link")));
     collectionModel.add(WebMvcLinkBuilder
         .linkTo(methodOn(VideoController_Level3.class).getAllVideos())
         .withRel("self"));
