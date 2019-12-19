71644a267b16cbe9541ac77c49ea9b477fbd250d
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-12-19 21:07:58 +0100

    Adds get meethods in PlaylistController_Level3

--- src/main/java/com/abevilacqua/youdude/controller/level_3/PlaylistController_Level3.java
+++ src/main/java/com/abevilacqua/youdude/controller/level_3/PlaylistController_Level3.java
@@ -1,9 +1,26 @@
 package com.abevilacqua.youdude.controller.level_3;
 
+import com.abevilacqua.youdude.controller.dto.PageImplDTO;
+import com.abevilacqua.youdude.controller.dto.PlaylistDTO;
+import com.abevilacqua.youdude.model.Playlist;
+import com.abevilacqua.youdude.model.resource.PlaylistResource;
+import com.abevilacqua.youdude.model.resource.PlaylistResourceAssembler;
 import com.abevilacqua.youdude.service.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+import static com.abevilacqua.youdude.controller.dto.PageImplDTO.pageMapper;
+import static com.abevilacqua.youdude.controller.dto.PlaylistDTO.mapper;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/level3/playlists", produces = "application/hal+json")
@@ -14,5 +31,29 @@ public class PlaylistController_Level3 {
   @Autowired
   public PlaylistController_Level3(final PlaylistService playlistService) {
     this.playlistService = playlistService;
+  }
+
+  @GetMapping
+  public ResponseEntity<CollectionModel<PlaylistResource>> getAllPlaylists() {
+    CompletableFuture<List<Playlist>> playlists = playlistService.getAllPlaylists();
+
+    CollectionModel<PlaylistResource> collectionModel =
+        new PlaylistResourceAssembler().toCollectionModel(playlists.join());
+
+    collectionModel.add(WebMvcLinkBuilder
+        .linkTo(methodOn(PlaylistController_Level3.class))
+        .withRel("recents"));
+
+    return new ResponseEntity<>(collectionModel, HttpStatus.OK);
+  }
+
+  @GetMapping("/{user_id}")
+  public ResponseEntity<CollectionModel<PlaylistResource>> getPlaylistPerUser(@PathVariable("user_id") final long user_id) {
+    CompletableFuture<List<Playlist>> playlistsFromUser = playlistService.getAllFromUser(user_id);
+
+    CollectionModel<PlaylistResource> collectionModel =
+        new PlaylistResourceAssembler().toCollectionModel(playlistsFromUser.join());
+
+    return new ResponseEntity<>(collectionModel, HttpStatus.OK);
   }
 }
