03984b2aef326c84c54958292200511daf1fa028
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2020-02-27 21:07:01 +0100

    Updates todos

--- src/main/java/com/abevilacqua/youdude/controller/level_2/VideoController_Level2.java
+++ src/main/java/com/abevilacqua/youdude/controller/level_2/VideoController_Level2.java
@@ -20,7 +20,7 @@ import static com.abevilacqua.youdude.controller.dto.VideoDTO.mapper;
 @RequestMapping("/level2/videos")
 public class VideoController_Level2 {
 
-  // todo: RESTFul, Evo Suite, bug with the page/size/sort
+  // todo: Evo Suite, gRPC, RESTful
 
   private VideoService videoService;
 
