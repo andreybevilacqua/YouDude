d6d0cc2bdff6bce59dfe8377399576631382e204
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2020-06-13 20:08:12 +0200

    Uses PostgreSQL and Flyway. Adds tables creation.

--- src/main/java/com/abevilacqua/youdude/model/User.java
+++ src/main/java/com/abevilacqua/youdude/model/User.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.UUID;
 
-@Entity
+@Entity(name = "user_application")
 @Getter
 @AllArgsConstructor
 public final class User implements Comparable<User>{
